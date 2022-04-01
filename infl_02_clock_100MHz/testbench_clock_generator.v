f6c97a17c67f8924ae8b7e32f6816a4b2f371464
HyoyeonLee <60028945+HyoyeonLee@users.noreply.github.com>
2022-04-01 19:54:31 +0900

    Create testbench_clock_generator.v
    Execute the code on terminal with...
             1) xvlog *.v
             2) xelab testbench_clock_generator -debug wave -s testbench_clock_generator
             3) xsim testbench_clock_generator -gui -wdb simulate_xsim_testbench_clock_generator

---
+++ infl_02_clock_100MHz/testbench_clock_generator.v
@@ -0,0 +1,13 @@
+`timescale 1ns/1ps//time unit for numbers / precision
+module tb_clock_generator ; 
+reg clk; //data type = register
+always //loop
+	#5 clk =~clk;// invert every 5ns (T=10ns, f=100MHz)
+initial begin
+	clk=0; //start @ Low
+$display(“start [%d]”,$time);
+#100 // operate for 100ns
+$display(“End [%d]”,$time);
+$finish;
+end 
+endmodule
