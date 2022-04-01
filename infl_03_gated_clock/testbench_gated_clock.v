fce613f5fd1976d67cc7190942bac53498231ceb
HyoyeonLee <60028945+HyoyeonLee@users.noreply.github.com>
2022-04-01 20:02:34 +0900

    Create testbench_gated_clock.v
    Execute ...
                     1) xvlog *.v
                     2) xelab testbench_gated_clock -debug wave -s testbench_gated_clock
                     3) xsim testbench_gated_clock -gui -wdb simulate_xsim_testbench_gated_clock

---
+++ infl_03_gated_clock/testbench_gated_clock.v
@@ -0,0 +1,27 @@
+timescale 1ns/1ps
+module testbench_gated_clock;
+reg clock;
+reg clock_en;
+wire o_clock;
+always 
+	$5 clock=~clock;
+initial begin
+clock=0;
+clock_en=0;
+#100
+$display(“clock ON [%d]”,$time);
+clock_en=1;
+#100
+$display(“OFF [%d]”,$time);
+clock_en=0;
+#100
+$display(“Finish!”);
+$finish;
+end
+  //connect DUT's i/o ports (DUT : instance of the module "gated_clock"
+DUT_gated_clock DUT(
+.i_clock (clock),
+.i_clock_en (clock_en),
+.o_clock (o_clock)
+);
+endmodule
