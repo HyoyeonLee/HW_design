b54751e4af8d65b7b0ded4df263f52ec3b23dfbe
HyoyeonLee <60028945+HyoyeonLee@users.noreply.github.com>
2022-05-25 20:07:18 +0900

    Create testbench_PMW.v

---
+++ review_ch07_loops/testbench_PMW.v
@@ -0,0 +1,19 @@
+`timescale 1ns/1ps
+module testbench_PMW;
+reg clock,pmw;
+integer clock_period=10;
+integer pmw_period=20;
+always #(clock_period/2) clock=~clock;
+initial begin
+        clock=0;
+        pmw=0;
+        forever #(pmw_period/2) pmw=~pmw;
+end
+
+
+initial begin
+        $display("start pmw");
+        #((pmw_period/2)*10) $display("end");
+$finish;
+end
+endmodule
